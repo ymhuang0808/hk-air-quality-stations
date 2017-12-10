62710dcfb55a16ead3860d5d67ed451fe6c6257e
John Huang <ymhuang@fmbase.tw>
2017-12-10 09:39:09 +0800

    Convert the CSV format into simple JSON and GeoJSON format

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# hk-air-quality-stations
-Convert the HK air quality station data to JSON format
+# Hong Kong Air Quality Stations
+
+The tool converts the HK air quality station data to JSON format.
+
+## Install and build
+
+```
+$ npm install
+$ npm run build
+```
+
+## Data source
+
+The raw data from [Hong Kong GeoInfo Map](http://www1.map.gov.hk/gih3/index.jsp) is a CSV file. You can download it from "Map Tools" > "Data Download" > "Air Quality Monitoring Network of Hong Kong"
+
+## License
+
+The code of Hong Kong Air Quality Stations is licensed under MIT
---
+++ index.js
@@ -0,0 +1,45 @@
+let fs = require("fs")
+let csv = require("fast-csv")
+let dms2dec = require("dms2dec")
+let GeoJSON = require("geojson")
+
+let input = fs.createReadStream("raw/AQMN_EPD_20171107.csv", {
+    encoding: "ucs2"
+})
+
+let simpleJsonData = []
+let geoJsonTransformer = (data) => {
+    return GeoJSON.parse(data, { Point: ["coordinates.latitude", "coordinates.longitude"] })
+}
+let csvStream = csv.fromStream(input, {delimiter: "\t", headers: true})
+
+csvStream
+    .transform((data) => {
+        let dmsLat = data["LATITUDE"].split("-").map(Number)
+        let dmsLong = data["LONGITUDE"].split("-").map(Number)
+        let coordinates = dms2dec(dmsLat, "N", dmsLong, "E")
+
+        return {
+            ch_name: data["中文名稱"].split("空氣質素監測站")[0],
+            eng_name: data["ENGLISH NAME"].split(" Air Quality Monitoring Station")[0],
+            coordinates: {
+                longitude: coordinates[1],
+                latitude: coordinates[0]
+            }
+        }
+    })
+    .on("data", (data) => {
+        simpleJsonData.push(data)
+    })
+    .on("end", () => {
+        fs.writeFile("data/hk-stations.json", JSON.stringify(simpleJsonData), (err) => {
+            if (err) console.error("Failed to write into data/hk-stations.json")
+            console.log("Generated data/hk-stations.json")
+        })
+
+        let geojsonData = geoJsonTransformer(simpleJsonData)
+        fs.writeFile("data/hk-stations.geojson", JSON.stringify(geojsonData), (err) => {
+            if (err) console.error("Failed to write into data/hk-stations.geojson")
+            console.log("Generated data/hk-stations.geojson")
+        })
+    })
